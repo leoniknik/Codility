caddec68e4c5fbfa5a6d3e651b49d4e0ae07153b
VolodinK <VolodinK@binbank.ru>
2017-09-13 22:13:36 +0300

    Создан проект

---
+++ Codility/Codility/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Codility
+//
+//  Created by Кирилл Володин on 13.09.17.
+//  Copyright © 2017 Кирилл Володин. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
